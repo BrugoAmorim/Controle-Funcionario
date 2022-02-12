4d5d0d8f3afce59fb8a4853752752449f41395a7
BrugoAmorim <brunogomes.concomercial@gmail.com>
2022-02-12 00:04:26 -0300

    subindo atualizacoes

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -39,7 +39,7 @@ namespace backend
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
+            {   
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend v1"));
